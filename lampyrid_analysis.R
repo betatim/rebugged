d1fad193db80a50c0e8a458d425c7aeccf262f81
Tim Head <betatim@gmail.com>
2018-09-12 14:01:57 +0200

    Update lampyrid_analysis.R

--- lampyrid_analysis.R
+++ lampyrid_analysis.R
@@ -1,12 +1,4 @@
-#install needed packages
-
-install.packages("lubridate")
-install.packages("reshape2")
-install.packages("plyr")
-install.packages("ggplot2")
-install.packages("gridExtra")
-install.packages("vegan")
-install.packages("MASS")
+# Packages are installed install.R
 
 
 # lampyrid analysis
